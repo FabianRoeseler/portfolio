1a73fb391a5393dc2f4af236784c9429620d8bfc
crimme <kueppers.fabian@web.de>
2024-09-27 11:14:12 +0200

    page structure added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./shared/header/header.component";
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeroPageComponent } from './hero-page/hero-page.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { MyskillsComponent } from './myskills/myskills.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactmeComponent } from './contactme/contactme.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, HeroPageComponent, FooterComponent, AboutmeComponent, MyskillsComponent, PortfolioComponent, ContactmeComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
