c26965c2afed9d0ee3d25f73aa07170b6b877a5c
crimme <kueppers.fabian@web.de>
2024-11-14 14:58:11 +0100

    modal changes

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule, DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
+
 export class FooterComponent {
+  isModalOpen = false;
+
+  constructor(@Inject(DOCUMENT) private document: Document) {}
+
+  openModal() {
+    this.isModalOpen = true;
+    this.document.body.style.overflow = 'hidden'; // Prevent background scroll
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+    this.document.body.style.overflow = 'auto'; // Restore background scroll
+  }
 
 }
