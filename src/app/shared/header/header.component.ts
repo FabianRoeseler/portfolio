c299fc974c8c2a333e3e2edb7879a407cbe08b5e
crimme <kueppers.fabian@web.de>
2024-11-19 11:16:00 +0100

    nav rebuild

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,16 +1,30 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterModule } from '@angular/router';
+import { CommonModule, DOCUMENT } from '@angular/common';
+import { Component, Inject } from '@angular/core';
 
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
 
   isNavigationOpen = false;
+
+  isModalOpen = false;
+
+  constructor(@Inject(DOCUMENT) private document: Document) {}
+
+  openModal() {
+    this.isModalOpen = true;
+    this.document.body.style.overflow = 'hidden'; // Prevent background scroll
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+    this.document.body.style.overflow = 'auto'; // Restore background scroll
+  }
 
 }
